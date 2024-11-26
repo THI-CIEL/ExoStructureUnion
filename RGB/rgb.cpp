5924ed8e04a2b082721ea92c55214022934e9e41
Thibault Podechard <titi.podechard@gmail.com>
2024-11-26 10:33:06 +0100

    Réalisation du code (à finir) de RGBsizeof all / modifs

--- RGB/rgb.cpp
+++ RGB/rgb.cpp
@@ -6,7 +6,34 @@
 #include <conio.h>
 using namespace std;
 
+struct Srgb
+{
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+};
+
+union UColor
+{
+	unsigned int val;
+	Srgb components;
+	unsigned char tabCol[3];
+};
+
 int main()
 {
-	cout << "Voici le programme du fichier RGB :\n";
+	Srgb couleur;
+	UColor tableau;
+	cout << "Voici le programme du fichier RGB :\nVeuillez saisir les valeurs de la couleur que vous voulez afficher : ";
+	cout << "Valeur de rouge (en hexadecimal) : ";
+	cin >> hex >> couleur.r;
+	cout << "Valeur de vert (en hexadecimal) : ";
+	cin >> hex >> couleur.g;
+	cout << "Valeur de bleu (en hexadecimal) : ";
+	cin >> hex >> couleur.b;
+
+	tableau.tabCol[0] = couleur.r;
+	tableau.tabCol[1] = couleur.g;
+	tableau.tabCol[2] = couleur.b;
+	cout << hex << tableau.tabCol;
 }
