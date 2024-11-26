bf8d30571ccd850c3cc73c6130c99626b2ab00c9
Thibault Podechard <titi.podechard@gmail.com>
2024-11-26 09:03:32 +0100

    Déclarer les variables / structures

--- Transgerbeur/Transgerbeurcpp.cpp
+++ Transgerbeur/Transgerbeurcpp.cpp
@@ -6,7 +6,28 @@
 #include <conio.h>
 using namespace std;
 
+union contenu;
+struct palletteCasier
+{
+	int poids;
+	char reference[20];
+};
+struct rouleauCasier
+{
+	int numero;
+	float longueur;
+};
+struct casier
+{
+	enum EEtat;
+};
+
 int main()
 {
 	cout << "Voici le programme du fichier Transgerbeur :\n";
+	int px, py;
+	int casier = {};
+	rouleauCasier ro1;
+	palletteCasier pa1;
+	
 }
